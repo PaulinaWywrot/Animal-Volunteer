5f23c8fbdaf7f6ddbc90d6a493483dcccfe8dbc0
PaulinaWywrot <wywrot.paula@gmail.com>
2023-09-24 17:22:27 +0100

    update get request/install pg/dotenv

--- server/server.js
+++ server/server.js
@@ -4,9 +4,22 @@ const port = process.env.PORT || 3007;
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
+const dotenv = require("dotenv");
+dotenv.config();
+const { Pool } = require("pg");
+const db = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
-app.get("/", async (req, res) => {
-  res.send("App is working");
+app.get("/sessions", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM sessions ORDER BY id");
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "failed to fetch sessions" });
+  }
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
