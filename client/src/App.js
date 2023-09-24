219f9d57fb4baa498dec9080cdebf8fd1e60a1e3
PaulinaWywrot <wywrot.paula@gmail.com>
2023-09-24 11:56:07 +0100

    removed logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="header">PPP</header>
+      <header className="header"></header>
     </div>
   );
 }
