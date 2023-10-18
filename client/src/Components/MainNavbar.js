578ba5d8c302e31de542fccc18d88b7735a38f1d
PaulinaWywrot <wywrot.paula@gmail.com>
2023-10-18 11:09:24 +0100

    fix navbar css

--- client/src/Components/MainNavbar.js
+++ client/src/Components/MainNavbar.js
@@ -3,21 +3,37 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-const MainNavbar = ({ setViewPage }) => {
+const MainNavbar = ({ viewPage, setViewPage }) => {
+  const handleNavbarBrandClick = () => {
+    setViewPage("About");
+  };
+
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#about" onClick={() => setViewPage("About")}>
+        <Navbar.Brand href="#about" onClick={handleNavbarBrandClick}>
           About Us
         </Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#book" onClick={() => setViewPage("Calendar")}>
+          <Nav.Link
+            href="#book"
+            onClick={() => setViewPage("Calendar")}
+            active={viewPage === "Calendar"}
+          >
             Book a Session
           </Nav.Link>
-          <Nav.Link href="#volunteer" onClick={() => setViewPage("Volunteer")}>
+          <Nav.Link
+            href="#volunteer"
+            onClick={() => setViewPage("Volunteer")}
+            active={viewPage === "Volunteer"}
+          >
             Volunteer
           </Nav.Link>
-          <Nav.Link href="#manager" onClick={() => setViewPage("Manager")}>
+          <Nav.Link
+            href="#manager"
+            onClick={() => setViewPage("Manager")}
+            active={viewPage === "Manager"}
+          >
             Manager
           </Nav.Link>
         </Nav>
